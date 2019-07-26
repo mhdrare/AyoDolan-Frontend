f5923d49d7ef66b6752c5edb1a4303c1adeb1d29
kdrsm <eryx466@gmail.com>
2019-07-26 08:51:16 +0700

    qrcode fixing

--- android/app/src/main/java/com/ayodolan/MainApplication.java
+++ android/app/src/main/java/com/ayodolan/MainApplication.java
@@ -3,6 +3,7 @@ package com.ayodolan;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new ReactNativeOneSignalPackage(),
           new VectorIconsPackage(),
           new MapsPackage(),
